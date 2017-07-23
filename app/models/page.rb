8430583fcc5664dc58dd112ba689cbf7543cc97d
Morita Jun <morita.jn0124@gmail.com>
2017-07-23 17:03:07 +0900

    add sethtml function and test

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,15 @@
 class Page < ApplicationRecord
+  validates :name, presence: true
+  validates :url, presence: true
+  validates :html, presence: true
+
+  def set_html
+    # HTMLの取得
+    charset = nil
+    html = open(self.url) do |f|
+      charset = f.charset
+      f.read
+    end
+    self.html = html
+  end
 end
--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "正常な値が設定された場合、正常にページが登録出来ること" do
+    page = Page.new
+    page.name = "hoge"
+    page.url = "https://www.google.co.jp/"
+    page.set_html
+    assert page.save
+  end
+
+  test "名前未設定でページを投稿した際にエラーとなること" do
+    page = Page.new
+    page.url = "hoge"
+    assert_not page.save
+  end
+
+  test "URL未設定でページを投稿した際にエラーとなること" do
+    page = Page.new
+    page.name = "hoge"
+    assert_not page.save
   end
 end
