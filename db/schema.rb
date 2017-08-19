17b0257416db6e80b87b0ffda4891239d095719e
Morita Jun <morita.jn0124@gmail.com>
2017-08-19 15:54:09 +0900

    未読の場合はアイコンを出すように修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813030402) do
+ActiveRecord::Schema.define(version: 20170819061646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170813030402) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read"
     t.index ["page_id"], name: "index_favorites_on_page_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
