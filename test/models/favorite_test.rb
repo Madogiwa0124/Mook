7e0b482c6f6a4a70d93307c6e9eb43f28ddf1cf6
Morita Jun <morita.jn0124@gmail.com>
2017-08-26 21:07:23 +0900

    モデルのテストを整理した

--- test/models/favorite_test.rb
+++ test/models/favorite_test.rb
@@ -2,6 +2,14 @@ require 'test_helper'
 
 class FavoriteTest < ActiveSupport::TestCase
   test "指定したuser_idとpage_idを持つお気入りTBLのレコードが作成されること" do
-    # todo: TESTを書く。
+    favorite = Favorite.new
+    favorite.user_id = 1
+    favorite.page_id = 2
+    assert favorite.save
+  end
+
+  test "指定したuser_idとpage_idを持つお気入りTBLのレコードを削除出来ること" do
+    favorite = Favorite.all.first
+    assert favorite.destroy
   end
 end
