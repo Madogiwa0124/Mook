bd20cb62730a13b46ab452672b5cb557389cfc0d
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2017-08-26 21:07:23 +0900

    モデルのテストを整理した

--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
   fixtures :users
+  fixtures :pages
+  fixtures :favorites
 
   test "正常な値が設定された場合、正常にページが登録出来ること" do
     page = Page.new
@@ -15,12 +17,14 @@ class PageTest < ActiveSupport::TestCase
   test "名前未設定でページを投稿した際にエラーとなること" do
     page = Page.new
     page.url = "hoge"
+    page.user_id = users(:one)
     assert_not page.save
   end
 
   test "URL未設定でページを投稿した際にエラーとなること" do
     page = Page.new
     page.name = "hoge"
+    page.user_id = users(:one)
     assert_not page.save
   end
 
@@ -28,12 +32,14 @@ class PageTest < ActiveSupport::TestCase
     # 1件目
     page = Page.new
     page.name = "hoge"
+    page.user_id = users(:one)
     page.url = "https://www.google.co.jp/"
     page.html = page.get_html(page.url)
     page.save
     # 2件目
     page = Page.new
     page.name = "hoge"
+    page.user_id = users(:one)
     page.url = "https://www.google.co.jp/"
     page.html = page.get_html(page.url)
     assert_not page.save
@@ -42,9 +48,20 @@ class PageTest < ActiveSupport::TestCase
   test "HTMLが取得できなかった場合にエラーとなること" do
     page = Page.new
     page.name = "hoge"
+    page.user_id = users(:one)
     page.url  = "https://hogehogehogehogehoegoheoge"
     page.html = page.get_html(page.url)
     assert_not page.save
+  end
+
+  test "ページの削除が行えること" do
+    page = Page.new
+    page.name = "hoge"
+    page.url = "https://www.google.co.jp/"
+    page.html = page.get_html(page.url)
+    page.user_id = users(:one).id
+    page.save
+    assert Page.all.first.destroy
   end
 
   test "pageのHTMLに変更が返って来たときに変更を検知出来ること" do 
@@ -56,7 +73,7 @@ class PageTest < ActiveSupport::TestCase
     page.save
     page.url = "https://www.yahoo.co.jp/"
     page.html = page.get_html(page.url)
-    assert_not page.html == Page.find(page.id).html
+    assert_not_equal page.html, Page.find(page.id).html
   end
 
   test "検索条件に合致したページのみが返却されること" do
@@ -68,10 +85,32 @@ class PageTest < ActiveSupport::TestCase
       page.user_id = users(:one).id
       page.save
     end
-    assert Page.search("0").count == 1
+    assert_equal Page.search("0").count, 1
   end
 
-  test "お気に入り登録済のページのみが検索されること" do
-    # TODO:テストを書く
+  test "お気に入り登録済のページが返却されること" do
+    user = users(:one)
+    assert_equal Page.favorited_pages(user).count, 1
   end
+
+  test "指定したキーワードを持つページが検索出来ること" do
+    key = "bing"
+    pages = Page.search(key)
+    result = pages.map{ |p| p.url.include?(key) }
+    assert_equal result.uniq.count, 1
+    assert result.uniq[0]
+  end
+
+  test "お気にいり済ページを判定出来ること" do
+    assert pages(:one).is_favorited?(users(:one))
+  end
+
+  test "お気入り登録済のページの未読チェックが出来ること" do
+    assert_not pages(:one).is_read?(users(:one))
+  end
+
+  test "お気に入り未登録のページは既読として扱われること" do
+    assert pages(:two).is_read?(users(:one))
+  end
+
 end
