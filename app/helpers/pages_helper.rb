697057dfed54d4d06bdeece24a869df7736927f3
Morita Jun <morita.jn0124@gmail.com>
2017-09-15 23:37:34 +0900

    コメントのリプライ機能を追加

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,22 @@ module PagesHelper
   def format_date(date)
     date.strftime("%Y年%m月%d日 %H:%M")
   end
+
+  def format_comment(content)
+    # 返信用の文字列を定義
+    reply = { char: '>>', escaped: '&gt;&gt;' }
+    # コメント本文のHTMLをエスケープ後、返信用の文字列のみをHTMLとして再定義
+    content = h(content).gsub(reply[:escaped], reply[:char])
+    # 返信先リンク部の取得
+    content_arry = content.split(" ")
+    target = content_arry.select{ |c| c.include?(reply[:char]) }
+    target.each do |t|
+      # 返信先リンクHTMLの生成
+      to = t.match(/#{reply[:char]}\d+/).to_s
+      link_html = link_to to, "#comment_#{to.gsub(reply[:char], '')}"
+      # 本文の返信部を返信先リンクHTMLで置換
+      content = content.gsub(to, link_html)
+    end
+    content
+  end
 end
