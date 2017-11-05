927946735c3b3a827daa0659e53f560020599f56
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2017-11-05 21:29:10 +0900

    コメントの内容だけを返却するようにした

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -47,7 +47,11 @@ class PagesController < ApplicationController
     @comments = @comments.page(params[:page]).per(10)
     respond_to do |format|
       format.html
-      format.json { render json: @comments }
+      format.json do 
+        ret = @comments.map(&:attributes)
+        ret = ret.map{ |c| c["content"] }
+        render json: ret
+      end
     end
   end
 
