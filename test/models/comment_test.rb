261eb5616a33fd19211262b659bc45388e9bfb29
Morita Jun <morita.jn0124@gmail.com>
2018-01-05 17:45:12 +0900

    コメントのテストコードの作成、fixutureの修正

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+  fixtures :pages
+  test "指定した値を持つコメントが作成出来ること" do
+    comment = Comment.new
+    comment.page_id = pages(:one).id
+    comment.user_id = users(:one).id
+    comment.content = "hoge"
+    assert comment.save
+  end
+
+  test "指定した値でコメントが修正出来ること" do
+    comment = Comment.find(comments(:one).id)
+    comment.content = "fuga"
+    assert comment.save
+  end
+  
+  test "指定したコメントが削除出来ること" do
+    comment = Comment.find(comments(:one).id)
+    assert comment.destroy
+  end
 end
