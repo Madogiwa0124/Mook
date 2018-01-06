55d0a9e622527e462a99ad772a3835b2e92f4993
Morita Jun <morita.jn0124@gmail.com>
2018-01-06 21:43:41 +0900

    画像のメソッドをApplcationHelperに移行

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,15 @@ module ApplicationHelper
       twitter_card[:url] = page.url
       twitter_card[:title] = 'Moookからオススメページのお知らせ'
       twitter_card[:description] = "タイトル：#{page.name}、タグ：#{page.tag_list.join(',')}"
+      twitter_card[:image] = set_page_img(page.image_src)
     else
       twitter_card[:url] = 'https://moook.herokuapp.com/pages'
       twitter_card[:title] = 'Webページ更新管理ツール「Moook」'
       twitter_card[:description] = 'いつもの更新いつもの更新確認、Moookを使えばお気に入りのページの更新を見逃しません。'
+      twitter_card[:image] = 'https://raw.githubusercontent.com/Madogiwa0124/Moook/master/app/assets/images/favicon.png'
     end
     twitter_card[:card] = 'summary'
     twitter_card[:site] = '@Madogiwa_Boy'
-    twitter_card[:image] = 'https://raw.githubusercontent.com/Madogiwa0124/Moook/master/app/assets/images/favicon.png'
     twitter_card
   end
 
@@ -29,5 +30,23 @@ module ApplicationHelper
       EOF
     end
     html.html_safe
+  end
+
+  def set_page_img(image_src)
+    if image_src_check(image_src)
+      image_src
+    else
+      'https://github.com/Madogiwa0124/Moook/blob/master/app/assets/images/no_image.png?raw=true'
+    end
+  end
+
+  private
+
+  def image_src_check(image_src)
+    if image_src
+      !!image_src.match(/(http|https).*(JPG|jpg|jpeg|gif|png|svg)/)
+    else
+      false
+    end
   end
 end
