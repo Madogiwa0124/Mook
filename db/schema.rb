a76329440575e48c5c6c1ddfc741038e4798d88f
Morita Jun <morita.jn0124@gmail.com>
2018-02-12 21:29:21 +0900

    前回HTMLカラム追加のmigrationファイルを作成し、migrationを実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107173536) do
+ActiveRecord::Schema.define(version: 20180212122754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180107173536) do
     t.integer "user_id"
     t.string "image_src"
     t.datetime "page_update_date"
+    t.string "before_html"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
